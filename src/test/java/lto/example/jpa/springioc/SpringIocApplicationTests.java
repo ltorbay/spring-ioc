811f1b7d15ecdceb92968d13b0f0e0ac9145b92d
ltorbay <louistorbay@gmail.com>
2019-10-19 18:17:40 +0200

    initial commit

---
+++ src/test/java/lto/example/jpa/springioc/SpringIocApplicationTests.java
@@ -0,0 +1,13 @@
+package lto.example.jpa.springioc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
